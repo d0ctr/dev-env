b6cbd6a3121a95b8e4452b0202f024d79ed9f880
Danila Vasilev <33842017+d0ctr@users.noreply.github.com>
2024-01-26 19:16:40 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+NO_FORMAT="\033[0m"
+C_YELLOW="\033[38;5;11m"
+C_GREEN="\033[38;5;2m"
+
+
+spin() {
+  local spin_chars="⡿⢿⣻⣽⣾⣷⣯⣟"
+  local i=0
+  while [ true ]; do
+    printf "\rInstalling ${C_YELLOW}${spin_chars:i++%8:1}${NO_FORMAT} "
+    sleep 0.1
+  done
+}
+
 # Define repository URL
 repository="https://github.com/d0ctr/dev-env"
 
 # Define destination path
 destination_path="$HOME/.local/bin"
 
+spin &
+spinner=$!
+
 # Create destination folder if it doesn't exist
 mkdir -p "$destination_path"
 
 # Clone the repository
-git clone "$repository" temp_repo
+git clone "$repository" temp_repo 2> /dev/null
 
 # Move to the repository folder
 cd temp_repo || exit
@@ -23,3 +40,6 @@ cd ..
 
 # Cleanup - remove the cloned repository
 rm -rf temp_repo
+
+kill $spinner &> /dev/null
+echo -e "\b\b${C_GREEN}✓${NO_FORMAT}"
